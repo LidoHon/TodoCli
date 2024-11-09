294c2a75de46db97e22809c75c9f94dbb246fdc2
LidoHon <lidohon10@gmail.com>
2024-11-09 10:36:40 +0300

    feat: add CLI-based todo app
    - Implement basic CRUD functionality for todos
    - Support listing, adding, updating, and deleting todos
    - Add CLI flags for todo management commands
    - Ensure data persistence through a local txt file

--- internal/interfaces/cli.go
+++ internal/interfaces/cli.go
@@ -1,3 +1,99 @@
 package interfaces
 
-// cli interface to handle user interactions
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+
+	usecase "github.com/LidoHon/TodoCli/internal/useCase"
+)
+
+// ANSI color codes
+const (
+	Reset   = "\033[0m"
+	Bold    = "\033[1m"
+	Underline = "\033[4m"
+	Red     = "\033[31m"
+	Green   = "\033[32m"
+	Blue    = "\033[34m"
+	Purple  = "\033[35m"
+	Cyan    = "\033[36m"
+
+
+)
+
+
+func RunCli(todoService *usecase.TodoService){
+	scanner :=bufio.NewScanner(os.Stdin)
+
+	for{
+		fmt.Println(Purple+ Bold+ "\n choose a command : create | update | delete | listAll | listOne | quit: " + Reset)
+
+		scanner.Scan()
+		input := scanner.Text()
+
+		switch input {
+			case "create":
+				fmt.Print(Blue+ "enter todo title: " +Reset)
+				scanner.Scan()
+				title := scanner.Text()
+				err := todoService.CreateTodo(title)
+				if err != nil{
+					fmt.Println(err)
+				}else{
+					fmt.Println( Green + "todo created successfully" + Reset)
+				}
+
+			case "update":
+				fmt.Print(Blue +"enter todo id to update: " + Reset)
+				scanner.Scan()
+				id,_ := strconv.Atoi(scanner.Text())
+				if err := todoService.UpdateTodo(id); err != nil{
+					fmt.Println(err)
+				}
+				fmt.Println(Green + "todo marked completed successfully" + Reset)
+				
+
+			case "delete":
+				fmt.Print(Blue + "enter todo id to delete: " + Reset)
+				scanner.Scan()
+				id,_ := strconv.Atoi(scanner.Text())
+				if err := todoService.DeleteTodos(id); err != nil{
+					fmt.Println(err)
+				}
+				fmt.Println(Green + "todo deleted successfully" + Reset)
+				
+
+			case "listAll":
+				todos := todoService.GetTodo()
+				fmt.Println("todos: ")
+				for _, todo := range todos{
+					status := "not completed"
+					if todo.Completed{
+						status = "completed"
+					}
+					fmt.Printf(Cyan + "ID: %d, Title: %s, Status: %s"+Reset+"\n", todo.ID, todo.Title, status + Reset)
+				}
+
+			case "listOne":
+				fmt.Print(Blue + "enter todo id to get: " + Reset)
+				scanner.Scan()
+				id,_ := strconv.Atoi(scanner.Text())
+				todo, err := todoService.GetTodoById(id)
+				if err != nil{
+					fmt.Println(err)
+				}
+					fmt.Printf(Cyan+"ID: %d, Title: %s, Completed: %t"+Reset+"\n", todo.ID, todo.Title, todo.Completed)
+
+				
+
+			case "quit":
+				return
+
+			default:
+				fmt.Println(Red + "invalid command" + Reset)
+				
+		}
+	}
+}
--- internal/repo/file.go
+++ internal/repo/file.go
@@ -1,3 +1,59 @@
 package repo
 
-// manages saving and  loading todos from a file
+import (
+	"bufio"
+	"encoding/json"
+	"os"
+
+	"github.com/LidoHon/TodoCli/internal/model"
+)
+
+type FileStorage struct{
+	fileName string
+}
+
+// so basically what the bellow constructor function would do is create a new fileStorage object and return it
+func NewFile(fileName string) *FileStorage{
+	return &FileStorage{fileName: fileName}
+}
+
+
+// save to-dos to the file
+func (fs *FileStorage) SaveTodos(todos []model.Todo) error {
+	file , err := os.Create(fs.fileName)
+	if err != nil{
+		return err
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	encoder := json.NewEncoder(writer)
+
+	if err :=encoder.Encode(todos); err !=nil{
+		return err
+	}
+	return writer.Flush()
+}
+
+
+// load to-dos
+
+func (fs *FileStorage) LoadTodos()([]model.Todo, error){
+	file, err := os.Open(fs.fileName)
+
+	if err !=nil{
+		if os.IsNotExist(err){
+			return []model.Todo{}, nil
+		}
+		return nil, err
+	}
+	defer file.Close()
+
+	var todos []model.Todo
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&todos); err !=nil{
+		return nil, err
+	}
+	return todos, nil
+}
+
